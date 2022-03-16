e3223d01f3d0fee66c3ec861d7795b02d99977d7
Borisov-Ilya <borisov.ilya22@gmail.com>
2022-03-16 20:53:46 +0300

    add HW - 06 (useState)

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,27 @@ import React from 'react'
 import './App.css'
 import {Accordion} from './components/Accordion/Accordion'
 import {Rating} from './components/Rating/Rating'
+import OnOff from './components/OnOff/OnOff'
+import {UncontrolledAccordion} from './components/UncontrolledAccordion/UncontrolledAccordion'
+import {UncontrolledRating} from './components/UncontrolledRating/UncontrolledRating'
 
 export const App = () => {
     return (
         <div className="App">
-            <Accordion titleValue={'Menu'} collapsed={true}/>
-            <Accordion titleValue={'Users'} collapsed={false}/>
+            <OnOff/>
 
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            {/*<Accordion titleValue={'Menu'} collapsed={true}/>*/}
+            {/*<Accordion titleValue={'Users'} collapsed={false}/>*/}
+            <UncontrolledAccordion titleValue={'Menu'}/>
+            <UncontrolledAccordion titleValue={'Users'}/>
+
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+
+            <UncontrolledRating/>
         </div>
     )
 }
